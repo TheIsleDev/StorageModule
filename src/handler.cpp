9a03d7030c88905a1dd8aad1b0e3934a570b7371
BlackCrystalic <39885003+BlackCrystalic@users.noreply.github.com>
2026-07-05 20:37:38 +0500

    Some improvements

--- src/handler.cpp
+++ src/handler.cpp
@@ -21,47 +21,56 @@
 namespace StorageSystemComponent {
 	using namespace RC::Unreal;
 
-	static UClass* _PlayerControllerBaseClass = nullptr;
-	static FProperty* _PlayerControllerPawn = nullptr;
-	static FProperty* _PlayerSteamId = nullptr;
+	static UClass* PlayerControllerBaseClass{};
+	static FProperty* PlayerControllerPawn{};
+	static FProperty* PlayerSteamID{};
 
-	static UClass* _DinoClass = nullptr;
-	static FProperty* _DinoClassID = nullptr;
+	static UClass* DinoClass{};
+	static FProperty* DinoClassID{};
 
-	static UFunction* _GetCharacterData = nullptr;
+	static UClass* CharacterClass{};
+
+	static UFunction* _GetCharacterData{};
 	static uint32 _GetCharacterData_BufferSize;
-	static FProperty* _GetCharacterData_PropCharacter = nullptr;
-	static FProperty* _GetCharacterData_PropForceSafe = nullptr;
-	static FProperty* _GetCharacterData_ReturnProperty = nullptr;
+	static FProperty* _GetCharacterData_PropCharacter{};
+	static FProperty* _GetCharacterData_PropForceSafe{};
+	static FProperty* _GetCharacterData_ReturnProperty{};
 
-	static UFunction* _PlayerDataToString = nullptr;
+	static UFunction* _PlayerDataToString{};
 	static uint32 _PlayerDataToString_BufferSize;
-	static FProperty* _PlayerDataToString_PropPlayerData = nullptr;
-	static FProperty* _PlayerDataToString_ReturnProperty = nullptr;
+	static FProperty* _PlayerDataToString_PropPlayerData{};
+	static FProperty* _PlayerDataToString_ReturnProperty{};
 
 	static StorageSystemConfiguration::DataOfGetCharacterData GetCharacterData{};
 	static StorageSystemConfiguration::DataOfPlayerDataToString PlayerDataToString{};
 	static StorageSystemConfiguration::DataOfStringToPlayerData StringToPlayerData{};
 	static StorageSystemConfiguration::DataOfAddSpawnRequest AddSpawnRequest{};
 
-	static IsleStructs::UTISaveManager* _TISaveManager = nullptr;
+	static IsleStructs::UTISaveManager* _TISaveManager{};
 
-	static UFunction* _TryToRespawn = nullptr;
-	static UFunction* _SetHealth = nullptr;
-	static UFunction* _WaitAndDestroyCorpse = nullptr;
+	static UFunction* _TryToRespawn{};
+	static UFunction* _SetHealth{};
+	static UFunction* _DestroyCorpse{};
 
-	static FProperty* _FuncParamPropChatMode = nullptr;
-	static FProperty* _FuncParamPropMessage = nullptr;
+	static FProperty* _FuncParamPropChatMode{};
+	static FProperty* _FuncParamPropMessage{};
+
+	static UObject* GameMode{};
 
 	auto SaveDino(IsleStructs::ATIPlayerController* Player) -> void {
-		IsleStructs::APawn* Pawn = *_PlayerControllerPawn->ContainerPtrToValuePtr<IsleStructs::APawn*>(Player);
-		if (!Pawn || !Pawn->IsA(_DinoClass)) return;
+		if (!GameMode) {
+			GameMode = UObjectGlobals::FindFirstOf(STR("BP_SurvivalGameMode_C"));
+			if (!GameMode) return;
+		};
 
-		FString SteamId = *_PlayerSteamId->ContainerPtrToValuePtr<FString>(Player);
+		IsleStructs::APawn* Pawn = *PlayerControllerPawn->ContainerPtrToValuePtr<IsleStructs::APawn*>(Player);
+		if (!Pawn || !Pawn->IsA(DinoClass)) return;
+
+		FString SteamID = *PlayerSteamID->ContainerPtrToValuePtr<FString>(Player);
 		IsleStructs::ATICharacterBase* Character = static_cast<IsleStructs::ATICharacterBase*>(Pawn);
-		int32 DinoId = *_DinoClassID->ContainerPtrToValuePtr<int32>(Character);
+		int32 DinoID = *DinoClassID->ContainerPtrToValuePtr<int32>(Character);
 		// Some more requirements, you can even add them to settings
-		Output::send(STR("Attempt to save dino for owner: {}, dinoid: {}"), *SteamId, DinoId);
+		Output::send(STR("Attempt to save dino for owner: {}, dinoid: {}"), *SteamID, DinoID);
 
 		StructsParams::FProcessEventParams FirstParams(GetCharacterData.Function, GetCharacterData.BufferSize);
 		FirstParams.Set(GetCharacterData.Character, Character);
@@ -73,28 +82,32 @@ namespace StorageSystemComponent {
 		_TISaveManager->ProcessEvent(PlayerDataToString.Function, SecondParams.Data());
 
 		FString Result = *SecondParams.GetAddress<FString>(PlayerDataToString.ReturnValue);
-		if(DataBaseConnector::SaveDino(SteamId, DinoId, Result, false)) {
+		if(DataBaseConnector::SaveDino(SteamID, DinoID, Result, false)) {
 			IsleStructs::FSetHealthParams SetHealthParams{0};
 			Character->ProcessEvent(_SetHealth, &SetHealthParams);
-			IsleStructs::FSetWaitAndDestroyCorpseParams CorpseParams{120};
-			Character->ProcessEvent(_WaitAndDestroyCorpse, &CorpseParams);
-			Output::send(STR("Dino removed from game, owner: {}, dinoid: {}"), *SteamId, DinoId);
+			IsleStructs::FTryToRespawnParams SetTryToRespawnParams{Player, SteamID};
+			GameMode->ProcessEvent(_TryToRespawn, &SetTryToRespawnParams);
+			Character->ProcessEvent(_DestroyCorpse, {});
+
+			Output::send(STR("Dino removed from game, owner: {}, dinoid: {}"), *SteamID, DinoID);
 		} else {
-			Output::send<LogLevel::Error>(STR("Failed to save dino for owner: {}, dinoid: {}"), *SteamId, DinoId);
+			Output::send<LogLevel::Error>(STR("Failed to save dino for owner: {}, dinoid: {}"), *SteamID, DinoID);
 		}
 	}
 
 	// Remember, if you try to load dino with ID that already exist in world, youll be fucked. Joking, it just wont load.
-	auto LoadDino(IsleStructs::ATIPlayerController* Player, int32 DinoId) -> void {
-		auto* GameMode = UObjectGlobals::FindFirstOf(STR("BP_SurvivalGameMode_C"));// Idk why I don't cache it, me is dum dum.
-		if (!GameMode) return;
+	auto LoadDino(IsleStructs::ATIPlayerController* Player, int32 DinoID) -> void {
+		if (!GameMode) {
+			GameMode = UObjectGlobals::FindFirstOf(STR("BP_SurvivalGameMode_C"));
+			if (!GameMode) return;
+		};
 
-		FString SteamId = *_PlayerSteamId->ContainerPtrToValuePtr<FString>(Player);
-		Output::send(STR("Attempt to load dino for owner: {}, dinoid: {}"), *SteamId, DinoId);
+		FString SteamID = *PlayerSteamID->ContainerPtrToValuePtr<FString>(Player);
+		Output::send(STR("Attempt to load dino for owner: {}, dinoid: {}"), *SteamID, DinoID);
 
 		FString Requested;
-		if(!DataBaseConnector::LoadDino(SteamId, DinoId, Requested)) {
-			Output::send<LogLevel::Error>(STR("Failed to load dino for owner: {}, dinoid: {}"), *SteamId, DinoId);
+		if(!DataBaseConnector::LoadDino(SteamID, DinoID, Requested)) {
+			Output::send<LogLevel::Error>(STR("Failed to load dino for owner: {}, dinoid: {}"), *SteamID, DinoID);
 			return;
 		};
 
@@ -111,7 +124,7 @@ namespace StorageSystemComponent {
 		StructsParams::FProcessEventParams SecondParams(AddSpawnRequest.Function, AddSpawnRequest.BufferSize);
 		void* Container = SecondParams.GetAddress<void>(AddSpawnRequest.SpawnData);
 		SecondParams.SetInContainer(Container, AddSpawnRequest.SpawnData_Controller, Player);
-		SecondParams.SetInContainer(Container, AddSpawnRequest.SpawnData_SteamId, SteamId);
+		SecondParams.SetInContainer(Container, AddSpawnRequest.SpawnData_SteamId, SteamID);
 		SecondParams.CopyFromAddress(Container, AddSpawnRequest.SpawnData_Class, Class);
 		SecondParams.SetInContainer(Container, AddSpawnRequest.SpawnData_bKeepActualLoc, false);// Only this one is safe to change out of all bools
 		SecondParams.SetInContainer(Container, AddSpawnRequest.SpawnData_bLoadOnly, false);// DO | DO | DO
@@ -123,14 +136,22 @@ namespace StorageSystemComponent {
 		SecondParams.Set(AddSpawnRequest.bAddHud, true);
 		SecondParams.Set(AddSpawnRequest.bLoadSaved, true);
 
-		IsleStructs::FTryToRespawnParams SetTryToRespawnParams{Player, SteamId};
-		GameMode->ProcessEvent(_TryToRespawn, &SetTryToRespawnParams);// Magic WORD! Sometimes you just have to belive me
+		IsleStructs::APawn* Pawn = *PlayerControllerPawn->ContainerPtrToValuePtr<IsleStructs::APawn*>(Player);
+		if (Pawn && Pawn->IsA(CharacterClass)) {
+			IsleStructs::ATICharacterBase* Character = static_cast<IsleStructs::ATICharacterBase*>(Pawn);
+			IsleStructs::FSetHealthParams SetHealthParams{0};
+			Character->ProcessEvent(_SetHealth, &SetHealthParams);
+			Character->ProcessEvent(_DestroyCorpse, {});
+		}
+
+		IsleStructs::FTryToRespawnParams SetTryToRespawnParams{Player, SteamID};
+		GameMode->ProcessEvent(_TryToRespawn, &SetTryToRespawnParams);// Magic WORD! Sometimes you just have to believe me
 		GameMode->ProcessEvent(AddSpawnRequest.Function, SecondParams.Data());
 
-		if (!DataBaseConnector::ConfirmLoadDino(SteamId, DinoId)) {// For now I don't care for tracking load, if it really loaded or not. You can add later to check Id field on dino, its enough
-			Output::send<LogLevel::Error>(STR("Failed to update dino status for owner: {}, dinoid: {}"), *SteamId, DinoId);
+		if (!DataBaseConnector::ConfirmLoadDino(SteamID, DinoID)) {// For now I don't care for tracking load, if it really loaded or not. You can add later to check Id field on dino, its enough
+			Output::send<LogLevel::Error>(STR("Failed to update dino status for owner: {}, dinoid: {}"), *SteamID, DinoID);
 		}
-		Output::send(STR("Loaded dino for owner: {}, dinoid: {}"), *SteamId, DinoId);
+		Output::send(STR("Loaded dino for owner: {}, dinoid: {}"), *SteamID, DinoID);
 	}
 
 	auto HandleChatMessage(UnrealScriptFunctionCallableContext& FuncContext) -> void {
@@ -146,27 +167,29 @@ namespace StorageSystemComponent {
 			SaveDino(static_cast<IsleStructs::ATIPlayerController*>(FuncContext.Context));
 		} else if (Message.starts_with(Prefix)) {
 			StringType IdString = Message.substr(FCString::Strlen(Prefix));
-			int32 DinoId;// Dont judge, 3 lines below this one is just LLM slopcode.
+			int32 DinoID;// Dont judge, 3 lines below this one is just LLM slopcode.
 			auto Utf8 = to_string(IdString);
-			auto [ptr, ec] = std::from_chars(Utf8.data(), Utf8.data() + Utf8.size(), DinoId);
+			auto [ptr, ec] = std::from_chars(Utf8.data(), Utf8.data() + Utf8.size(), DinoID);
 			if (ec != std::errc() || ptr != Utf8.data() + Utf8.size()) return;
-			LoadDino(static_cast<IsleStructs::ATIPlayerController*>(FuncContext.Context), DinoId);
+			LoadDino(static_cast<IsleStructs::ATIPlayerController*>(FuncContext.Context), DinoID);
 		}
 	}
 
-	static UFunction* GetChatMessage = nullptr;
-	static int32_t HookId;
+	static UFunction* GetChatMessage{};
+	static int32_t HookID{};
 	auto Initialize(StorageSystemConfiguration::StorageConfig Config) -> void {
 		if (!DataBaseConnector::Initialize(Config.Database)) {
 			Output::send<LogLevel::Error>(STR("DB connection failed, con string: {}"), to_wstring(Config.Database));
-		}
+		} else DataBaseConnector::PrepareStorage();
 
-		_PlayerControllerBaseClass = UObjectGlobals::StaticFindObject<UClass*>(nullptr, nullptr, STR("/Script/TheIsle.TIPlayerController"));
-		_PlayerControllerPawn = _PlayerControllerBaseClass->GetPropertyByNameInChain(STR("Pawn"));
-		_PlayerSteamId = _PlayerControllerBaseClass->GetPropertyByNameInChain(STR("SteamId"));
+		PlayerControllerBaseClass = UObjectGlobals::StaticFindObject<UClass*>(nullptr, nullptr, STR("/Script/TheIsle.TIPlayerController"));
+		PlayerControllerPawn = PlayerControllerBaseClass->GetPropertyByNameInChain(STR("Pawn"));
+		PlayerSteamID = PlayerControllerBaseClass->GetPropertyByNameInChain(STR("SteamId"));
 
-		_DinoClass = UObjectGlobals::StaticFindObject<UClass*>(nullptr, nullptr, STR("/Script/TheIsle.TIDinosaurBase"));
-		_DinoClassID = _DinoClass->GetPropertyByNameInChain(STR("ID"));
+		DinoClass = UObjectGlobals::StaticFindObject<UClass*>(nullptr, nullptr, STR("/Script/TheIsle.TIDinosaurBase"));
+		DinoClassID = DinoClass->GetPropertyByNameInChain(STR("ID"));
+
+		CharacterClass = UObjectGlobals::StaticFindObject<UClass*>(nullptr, nullptr, STR("/Script/TheIsle.TICharacterBase"));
 
 		GetCharacterData.Function = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TISaveManager:GetCharacterData"));
 		GetCharacterData.Initialize();
@@ -181,20 +204,20 @@ namespace StorageSystemComponent {
 
 		_TryToRespawn = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TIGameModeBase:TryToRespawn"));
 		_SetHealth = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TICharacterBase:SetHealth"));// Safe way to kill it
-		_WaitAndDestroyCorpse = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TICharacterBase:WaitAndDestroyCorpse"));// Safe way to clean up it
+		_DestroyCorpse = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TICharacterBase:DestroyCorpse"));
 
 		GetChatMessage = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TIPlayerController:GetChatMessage"));
 		_FuncParamPropChatMode = GetChatMessage->GetPropertyByNameInChain(STR("ChatMode"));
 		_FuncParamPropMessage = GetChatMessage->GetPropertyByNameInChain(STR("NoFilterMsg"));
 
-		HookId = GetChatMessage->RegisterPostHook(
+		HookID = GetChatMessage->RegisterPostHook(
 			[](UnrealScriptFunctionCallableContext& FuncContext, void*) -> void {HandleChatMessage(FuncContext);
 		});
 	}
 
 	auto Destroy() -> void {
 		DataBaseConnector::Destroy();
-		if (!HookId) return;// Idk if this is possible in prod to hit case where we dont get it, however there no safe checks down the road, so better to safe check here
-		GetChatMessage->UnregisterHook(HookId);
+		if (!HookID) return;// Idk if this is possible in prod to hit case where we dont get it, however there no safe checks down the road, so better to safe check here
+		GetChatMessage->UnregisterHook(HookID);
 	}
 }
